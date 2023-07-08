b70b497d4c8e7ef8404d5ca7a636b8c1c1756831
graemebates <graeme.gilmour.bates@gmail.com>
2023-07-08 00:19:25 -0700

    Updated templater so that it is way more convenient

--- include/Templater.hpp
+++ include/Templater.hpp
@@ -5,84 +5,95 @@
 #include "Logger.hpp"
 
 #ifndef TEMPLATER
-#define  TEMPLATER
+#define TEMPLATER
 
-namespace HTML{
-  // Things like, style, href, class etc...
-  class Attribute{
+namespace HTML {
+  // Forward declarations
+  class Node;
+  class Doc;
+
+  // Attribute class for HTML tags
+  class Attribute {
   public:
     std::string name;
     std::vector<std::string> values;
 
-    Attribute(std::string t_name){
-      name = t_name;
-    }
+    Attribute(std::string t_name) : name(t_name) {}
 
-    void add_value(std::string t_value){
+    void add_value(std::string t_value) {
       values.push_back(t_value);
     }
   };
-  // The actual html tags
-  class Node{
-  public:
-    // Here body is text like: <a>body</a>
-    // and content is inner tags like: <a><content>body</content></a>
-    std::string name, body;
+
+  // The actual HTML tags
+  class Node {
+    public:
+    std::string name;
+    std::string body;
     bool self_closing;
     std::vector<Node> content;
-    std::vector<Attribute>  attributes;
+    std::vector<Attribute> attributes;
+    Node(std::string t_name, bool is_self_closing = false)
+        : name(t_name), self_closing(is_self_closing) {}
 
-    Node(std::string t_name){
-      name = t_name;
-      self_closing = false;
-    }
-    Node(std::string t_name, bool is_self_closing) : Node(t_name){
-      self_closing = is_self_closing;
-    }
-    // Adding attributes like <a attribute="value value" />
-    void add_attribute(
-      std::string t_attribute,
-      std::vector<std::string> t_values
-    ){
+    // Method for adding attributes like <a attribute="value value" />
+    Node& operator()(std::string t_attribute, std::vector<std::string> t_values) {
       Attribute attr(t_attribute);
-      for(std::string value : t_values){
+      for (std::string value : t_values) {
         attr.add_value(value);
       }
       attributes.push_back(attr);
+      return *this;
     }
-    void add_body(std::string t_body){
-      body = t_body + "\n";
+
+    // Method for adding body content like <a>body</a>
+    Node& operator()(std::string t_body) {
+      body = t_body;
+      return *this;
     }
-    // Generate the open and close tag
-    // Note: for self closing, only use open.
-    std::string open(){
+
+    // Method for adding child nodes
+    Node& operator()(const Node& node) {
+      content.push_back(node);
+      return *this;
+    }
+
+    // Generate the opening tag
+    std::string open() const {
       std::stringstream out;
       out << "<" << name;
-      for(Attribute attr : attributes){
+      for (const Attribute& attr : attributes) {
         logger->log("Adding attribute");
-        out << " ";
-        out << attr.name << "=\"";
-        for(std::string attr_value : attr.values){
+        out << " " << attr.name << "=\"";
+        for (const std::string& attr_value : attr.values) {
           out << attr_value << " ";
         }
         out << "\"";
       }
-      if(self_closing) out << "/";
+      if (self_closing) {
+        out << "/";
+      }
       out << ">";
       return out.str();
     }
-    std::string close(){
-      std::stringstream out;
-      out << "</" << name << ">\n";
-      return out.str();
+
+    // Generate the closing tag
+    std::string close() const {
+      if (!self_closing) {
+        std::stringstream out;
+        out << "</" << name << ">\n";
+        return out.str();
+      }
+      return "";
     }
-    // Return string of html
-    std::string render(){
+
+    // Render the node and its child nodes as HTML string
+    std::string render() const {
       std::stringstream out;
       out << open();
-      if(!self_closing){
-        out << "\n" << body;
-        for(Node element : content){
+      if (!self_closing) {
+        out << body;
+        for (const Node& element : content) {
           out << element.render();
         }
         out << close();
@@ -90,38 +101,78 @@ namespace HTML{
       return out.str();
     }
   };
-  // Wrapper to create complete html page.
-  // Always access head and body with 0 and 1 respectivly in html.content[]
-  class Doc{
+
+  // Wrapper to create a complete HTML page
+  class Doc {
   private:
     std::string file_type;
     Node html;
     Node head; // html.content[0]
     Node body; // html.content[1]
+
   public:
-    Doc(): html("html"), head("head"), body("body"){
-      file_type = "<!DOCTYPE html>";
-      html.content.push_back(head);
-      html.content.push_back(body);
+    Doc()
+        : file_type("<!DOCTYPE html>"), html("html"), head("head"), body("body") {
+      html(head)(body);
     }
 
-    void add_to_head(Node t_content){
-      html.content[0].content.push_back(t_content);
+    // Method for adding content to the <head> tag
+    Doc& operator()(const Node& t_content) {
+      html.content[0](t_content);
+      return *this;
     }
 
-    void add_to_body(Node t_content){
-      html.content[1].content.push_back(t_content);
+    // Method for adding content to the <body> tag
+    Doc& operator[](const Node& t_content) {
+      html.content[1](t_content);
+      return *this;
     }
 
-    std::string render(){
+    // Render the HTML document as a string
+    std::string render() const {
       std::stringstream out;
-      out << file_type;
-      out << "\n";
-      out << html.render();
+      out << file_type << "\n" << html.render();
       return out.str();
     }
   };
 
+  // Convenience function to create an HTML tag with attributes and body
+  inline Node tag(
+    const std::string& name,
+    const std::string& t_attribute,
+    const std::vector<std::string> t_attributes,
+    const std::string& body) {
+    Node node(name);
+    node(t_attribute, t_attributes)(body);
+    return node;
+  }
+
+  // Convenience function to create an HTML tag with attributes
+  inline Node tag(
+    const std::string& name,
+    const std::pair<std::string, std::vector<std::string>&> t_attribute
+  ) {
+    Node node(name);
+    node(t_attribute.first, t_attribute.second);
+    return node;
+  }
+
+  // Convenience function to create an HTML tag with body
+  inline Node tag(const std::string& name, const std::string& body) {
+    Node node(name);
+    node(body);
+    return node;
+  }
+
+  // Convenience function to create a self-closing HTML tag
+  inline Node selfClosingTag(const std::string& name) {
+    return Node(name, true);
+  }
+
+  // Convenience function to create an HTML tag without attributes or body
+  inline Node tag(const std::string& name) {
+    return Node(name);
+  }
 }
 
 #endif
